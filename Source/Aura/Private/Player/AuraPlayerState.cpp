0b02776458c72c71a5b02c9a33fff9be6fca6f7b
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-18 19:59:06 -0400

    Player State Created

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+﻿// Assets provided by DruidMechanics. Copyright Jonathan Rampersad 2024
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
