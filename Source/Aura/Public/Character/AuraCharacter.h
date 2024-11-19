3ec76f0841edcf5bbb19860bdf6a43244545c227
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-19 18:41:15 -0400

    Init Ability Actor Info

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -26,4 +26,9 @@ private:
 
 public:
 	AAuraCharacter();
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+private:
+	void InitAbilityActorInfo();
 };
